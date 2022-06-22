5ead2f3e66bedaa114cf845936db4960a0e5870b
Konstantin Velly <konstantinvelly@gmail.com>
2022-06-22 09:24:40 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 25_000;
+        int miles = service.calculate(price);
+        System.out.println("Начисленные бонусные баллы:" + miles);
+    }
+}
